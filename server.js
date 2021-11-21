c6ad7ec17910fdcfa33644dd9401159d09a12cdf
Graham Preston <graham.preston@gmail.com>
2021-11-20 22:22:35 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+// Routes
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Services
+require('./services/movie-service')(app);
+require('./services/tweeter-service')(app);
+
+app.listen(4000);
